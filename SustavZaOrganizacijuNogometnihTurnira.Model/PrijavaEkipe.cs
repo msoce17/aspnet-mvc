338c2e49671a07a084fa135aec5d6cbbbdb208b1
Mateo <Mateo@DESKTOP-V6ORAON>
2026-04-16 13:05:12 +0200

    Revert "Merge branch 'master' of https://github.com/msoce17/aspnet-mvc"
    This reverts commit 70ca3bb7660f07c8434fcd2632427dd02b34b306, reversing
    changes made to 01c54bee5b817849dea2a0829ee67430901ce921.

---
+++ SustavZaOrganizacijuNogometnihTurnira.Model/PrijavaEkipe.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SustavZaOrganizacijuNogometnihTurnira.Model
+{
+    public class PrijavaEkipe
+    {
+        public int PrijavaEkipeId { get; set; }
+        public DateTime DatumPrijave { get; set; }
+        public bool Potvrdjeno { get; set; }
+
+        // Foreign Keys
+        public int TurnirId { get; set; }
+        public int EkipaId { get; set; }
+
+        // Virtual Properties
+        public virtual Turnir Turnir { get; set; }
+        public virtual Ekipa Ekipa { get; set; }
+    }
+}
